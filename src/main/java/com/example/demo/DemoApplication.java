52d3f7c0058cd3458d3bb9c031c0b9fd090b9239
jcardenasimasd <167337004+jcardenasimasd@users.noreply.github.com>
2024-08-29 18:48:52 +0000

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,34 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.Map;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	class DemoController{
+		@GetMapping("/hello")
+		public String hello() {
+			return "Hello render";
+		}
+
+		@PostMapping("/Weebhook")
+		public ResponseEntity<String> handleWebhook(@RequestBody Map<String, Object> event) {
+			System.err.println(event);
+			return ResponseEntity.ok(event.toString());
+		}
 	}
 
 }
