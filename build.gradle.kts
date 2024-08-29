e7f345c76dec0fc8dde6fc780bbe0b98048c94cb
jcardenasimasd <167337004+jcardenasimasd@users.noreply.github.com>
2024-08-29 19:45:39 +0000

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.3"
+	id("jakarta.persistence") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -19,6 +20,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("jakarta.persistence:jakarta.persistence-api")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
